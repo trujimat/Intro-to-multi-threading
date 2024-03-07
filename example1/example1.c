23d89040497f0cc1d76ada2ad5dbe4e6747475ce
trujimat <mateo.trujillo@fing.edu.uy>
2024-03-06 22:42:49 -0300

    first example

---
+++ example1/example1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> //Header file for sleep(). man 3 sleep for details.
+#include <pthread.h>
+
+void *example_function()
+{
+    printf("Entered the function \n");
+    sleep(2);
+    printf("About to exit the function \n");
+}
+
+int main()
+{
+    pthread_t thr1, thr2;
+    pthread_create(&thr1, NULL, example_function, NULL);
+    pthread_create(&thr2, NULL, example_function, NULL);
+    pthread_join(thr1, NULL);
+    pthread_join(thr2, NULL);
+}
